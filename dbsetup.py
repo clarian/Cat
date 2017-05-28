e6feefa4715258b09ff284346469908f48e0fe93
mental32 <m3nta1@yahoo.com>
2017-05-28 12:54:07 +0100

    A little addon

--- dbsetup.py
+++ dbsetup.py
@@ -3,3 +3,4 @@ Vitals = shelve.open("Catvitals")
 Vitals['build'] = {'version':0.2}
 Vitals['admins'] = []
 Vitals['botdetails'] = {'prefix':'!','description':'bot created by Timmy.'}
+Vitals.close()
